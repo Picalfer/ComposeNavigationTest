4c462dba280962e009cebbe8789aaf8acd509ef2
Arthur Kozhemyakin <trable228@icloud.com>
2025-11-24 11:03:34 +0300

    add padding

--- app/src/main/java/com/sport/composenavigationtest/screens/DetailsScreen.kt
+++ app/src/main/java/com/sport/composenavigationtest/screens/DetailsScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -27,7 +29,8 @@ fun DetailsScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
+            .padding(24.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center
     ) {
         Text(
